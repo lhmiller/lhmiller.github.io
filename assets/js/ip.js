92c654eb1dcb2dea8d741e177adecb8e5dc243aa
lhmiller <lhmiller@calpoly.edu>
2019-01-07 01:05:54 -0800

    updated ip page

--- assets/js/ip.js
+++ assets/js/ip.js
@@ -4,16 +4,26 @@ function ip() {
     'use strict';
     let ip = "https://ipinfo.io/" + location.hash.substr(1,location.hash.length);
     $.getJSON(ip, function(data){
+        var city = "", region = "";
         if(location.hash.length>1) {
             $('#ip').html(`${data.ip}`);
         } else {
-            $('#ip').html(`Your IP is ${data.ip}`);
+            $('#ip').html(`${data.ip}`);
         }
         if(data.hostname != undefined) {
             $('#hostname').html(`${data.hostname}`);
         }
+
+        if(data.city != "") {
+            city = `${data.city}, `;
+        }
+
+        if(data.region != "") {
+            region = `${data.region}, `;
+        }
+
         $('#org').html(`${data.org}`);
-        $('#loc').html(`${data.region}, ${data.country}`);
+        $('#loc').html(`${city} ${region} ${data.country}`);
         ipData = data;
     });
 }
